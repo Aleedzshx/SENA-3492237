611bf1d4fda7d5c3ba949ce5b4882077ff193b3b
Aleedzshx <217096621+Aleedzshx@users.noreply.github.com>
2026-09-01 14:38:09 -0500

    Actualizando otravez...

---
+++ Talleres2026/Parte_5/Taller_Parte_5.py
@@ -0,0 +1,19 @@
+#Dictionaries
+#
+#Ejercicio 41
+
+# students = {}
+# amount = int(input("\n¿Cuántos estudiantes quieres registrar? > "))
+
+# for i in range(amount):
+#     id = int(input(f"\nCodigo del estudiante {i + 1} > "))
+#     name = input(f"\nNombre del estudiante {i + 1} > ")
+#     students[id] = name
+
+# print("Listado de estudiantes")
+# for i in students:
+#     print(f"\n{i} -> {students[i]}")
+
+
+#Ejercicio 42
+#
